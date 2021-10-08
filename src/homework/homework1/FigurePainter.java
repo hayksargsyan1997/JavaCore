cd54834350889c801c60dfe35a425e912f2442b7
Hayko <hayksargsyan2019@bk.ru>
2021-10-08 16:30:16 +0400

    FigurePaintner

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -25,5 +25,37 @@ public class FigurePainter {
             }
             System.out.println();
         }
+        System.out.println();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 4; k > i; k--) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 3; k > i; k--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
     }
 }
+
